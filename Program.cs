ec6e2e3b159c7c6211ea95723d10261523739bfd
Gusttavo Soares <gustavosoares335@yahoo.com.br>
2022-05-24 14:02:54 -0300

    refactor: add computer repository save method

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using LabManager.Database;
 using Microsoft.Data.Sqlite;
 using LabManager.Repositories;
+using LabManager.Models;
 
 var databaseConfig = new DatabaseConfig();
 new DatabaseSetup(databaseConfig);
@@ -32,18 +33,10 @@ if (modelName == "Computer")
         int id = Convert.ToInt32(args[2]);
         string ram = args[3];
         string processor = args[4];
+        
+        var computer = new Computer(id, ram, processor);
+        computerRepository.Save(computer);  
 
-        var connection = new SqliteConnection("Data Source=database.db");
-        connection.Open();
-
-        var command = connection.CreateCommand();
-        command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor);";
-        command.Parameters.AddWithValue("$id", id);
-        command.Parameters.AddWithValue("$ram", ram);
-        command.Parameters.AddWithValue("$processor", processor);
-        command.ExecuteNonQuery();
-
-        connection.Close();
     }
 }
 
