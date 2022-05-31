d972609e163bb7980641f7699cffa4b26c44afd2
Gustavo Soares <gustavosoares335@yahoo.com.br>
2022-05-30 21:53:42 -0300

    enchan: add update method

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -68,5 +68,22 @@ class ComputerRepository // classe que fala com o banco de dados
         return computer;
     }
 
+    public Computer Update(Computer computer)
+    {
+        var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        connection.Open();
+
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = "UPDATE Computers SET  ram = $ram, processor = $processor WHERE id=$id;";
+        command.Parameters.AddWithValue("$id", computer.Id);
+        command.Parameters.AddWithValue("$ram", computer.Ram);
+        command.Parameters.AddWithValue("$processor", computer.Processor);
+        command.ExecuteNonQuery();
+
+        connection.Close();
+        return computer;
+    }
 }
 
